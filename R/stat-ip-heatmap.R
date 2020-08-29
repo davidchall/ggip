f18ae7678b5a49f581a94e991dd97ea0ab5642de
David Hall <david.hall.physics@gmail.com>
2020-08-29 16:48:34 -0700

    Only need `ip` aesthetic, layer will automatically find x and y

--- R/stat-ip-heatmap.R
+++ R/stat-ip-heatmap.R
@@ -1,18 +1,18 @@
 #' Heatmap of IP data
 #'
 #' @section Aesthetics:
-#' `stat_ip_heatmap()` understands the following aesthetics (required aesthetics
-#' are in bold):
-#'  - **`x`**
-#'  - **`y`**
+#' `stat_ip_heatmap()` understands the following aesthetics:
+#'  - `ip`: An [`ip_address`][`ipaddress::ip_address`] column
 #'  - `z`: Value passed to the summary function (required if `fun` is used)
 #'  - `fill`: Must use a computed variable (default: `after_stat(value)`)
-#'  - `ip`: [`ip_address`][`ipaddress::ip_address`] vector associated with `x`
-#'    and `y` aesthetics. Required if `ip_count` or `ip_propn` computed variable
-#'    is used.
+#'
+#' *Note:* Since this is a native ggip layer, it can accept an
+#' [`ip_address`][`ipaddress::ip_address`] column directly. It works together
+#' with [coord_ip()] to ensure IP data are correctly mapped to the `x` and `y`
+#' aesthetics.
 #'
 #' @section Computed variables:
-#' The following variables are available to [ggplot2::after_stat()]:
+#' The following variables are available to [`after_stat()`][ggplot2::after_stat()]:
 #'  - `value`: Value of summary statistic
 #'  - `count`: Number of observations
 #'  - `ip_count`: Number of unique addresses
@@ -38,35 +38,41 @@
 #'
 #' @inheritParams ggplot2::layer
 #' @inheritParams ggplot2::geom_point
+#' @param mapping Set of aesthetic mappings created by [`aes()`][ggplot2::aes()].
+#'   Note `stat_ip_heatmap()` does not inherit the default mapping specified in
+#'   [`ggplot()`][ggplot2::ggplot()], so these *must* be defined here.
 #' @param fun Summary function (see section below for details). If `NULL` (the
 #'   default), the number of observations is computed.
 #' @param fun.args A list of extra arguments to pass to `fun`
 #' @export
 stat_ip_heatmap <- function(mapping = NULL, data = NULL,
-                            ...,
                             fun = NULL, fun.args = list(),
-                            na.rm = FALSE, show.legend = NA,
-                            inherit.aes = TRUE) {
+                            na.rm = FALSE, show.legend = NA) {
+  if (is.null(mapping$ip)) {
+    abort("stat_ip_heatmap() requires `ip` aesthetic")
+  }
+
+  # extract {x, y, ip} aesthetics from ip aesthetic
+  ip_col <- as_name(mapping$ip)
+  mapping$x <- parse_expr(paste0(ip_col, "$x"))
+  mapping$y <- parse_expr(paste0(ip_col, "$y"))
+  mapping$ip <- parse_expr(paste0(ip_col, "$ip"))
+
   ggplot2::layer(
     stat = StatIpHeatmap, data = data, mapping = mapping, geom = "raster",
-    position = "identity", show.legend = show.legend, inherit.aes = inherit.aes,
+    position = "identity", show.legend = show.legend, inherit.aes = FALSE,
     params = list(
       na.rm = na.rm,
       fun = fun,
-      fun.args = fun.args,
-      ...
+      fun.args = fun.args
     )
   )
 }
 
 StatIpHeatmap <- ggplot2::ggproto("StatIpHeatmap", ggplot2::Stat,
-  required_aes = c("x", "y"),
+  required_aes = c("x", "y", "ip"),
 
-  default_aes = ggplot2::aes(
-    z = NULL,
-    ip = NULL,
-    fill = ggplot2::after_stat(value)
-  ),
+  default_aes = ggplot2::aes(z = NULL, fill = ggplot2::after_stat(value)),
 
   extra_params = c(
     "na.rm",
@@ -76,6 +82,10 @@ StatIpHeatmap <- ggplot2::ggproto("StatIpHeatmap", ggplot2::Stat,
   compute_layer = function(self, data, params, layout) {
     if (!is_CoordIp(layout$coord)) {
       abort("Must call coord_ip() when using ggip")
+    }
+
+    if (!is_ip_address(data$ip)) {
+      abort("stat_ip_heatmap requires `ip` aesthetic to be an ip_address vector")
     }
 
     # add coord to the params, so it can be forwarded to compute_group()
@@ -97,7 +107,7 @@ compute_ip_heatmap <- function(data, coord, fun, fun.args) {
 
   summarize_count <- is.null(fun)
   if (!summarize_count && !("z" %in% colnames(data))) {
-    abort("stat_ip_heatmap() requires `z` aesthetic when using non-default summary function")
+    abort("stat_ip_heatmap requires `z` aesthetic when using non-default summary function")
   }
 
   # summarize grid found in data
