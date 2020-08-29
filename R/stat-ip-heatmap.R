f306137ec62acf04ebe1835942871035a2e2d129
David Hall <david.hall.physics@gmail.com>
2020-08-29 10:46:04 -0700

    Count unique addresses Document summary function

--- R/stat-ip-heatmap.R
+++ R/stat-ip-heatmap.R
@@ -5,22 +5,40 @@
 #' are in bold):
 #'  - **`x`**
 #'  - **`y`**
-#'  - `z`: Value passed to the summary function (only required if `fun != "count"`).
+#'  - `z`: Value passed to the summary function (required if `fun` is used)
+#'  - `ip`: [`ip_address`][`ipaddress::ip_address`] vector
 #' @section Computed variables:
-#'  - `value`: Value of summary statistic
-#'  - `count`
-#'  - `proportion`
+#'  - `value`: Value of summary statistic (or `count` if `fun` not used)
+#'  - `count`: Number of observations
+#'  - `address_count`: Number of unique addresses
+#'  - `address_proportion`: Observed proportion of total addresses
 #' @section Summary function:
+#' The `data` might contain multiple rows per pixel of the heatmap, so we use
+#' a summary function to reduce this information to a single value to display.
+#' This function receives the `data` column specified by the `z` aesthetic
+#' and will also receive arguments specified by `fun.args`.
 #'
+#' The `fun` argument can be specified in multiple ways:
+#' \describe{
+#' \item{`NULL`}{If no summary function is provided, the number of observations
+#'   is computed. In this case, you don't need to specify the `z` aesthetic,
+#'   and the computed variables `value` and `count` will be equal.}
+#' \item{String}{The name of an existing function (e.g. `fun = "mean"`).}
+#' \item{Function}{Either provide an existing function (e.g. `fun = mean`) or
+#'   define a new function (e.g. `fun = function(x) sum(x^2)`).}
+#' \item{Formula}{A function can also be created from a formula. This uses `.x`
+#'   as the summarized variable (e.g. `fun = ~ sum(.x^2)`).}
+#' }
 #'
 #' @inheritParams ggplot2::layer
 #' @inheritParams ggplot2::geom_point
-#' @param fun Summary function
+#' @param fun Summary function (see section below for details). If `NULL` (the
+#'   default), the number of observations is computed.
 #' @param fun.args A list of extra arguments to pass to `fun`
 #' @export
 stat_ip_heatmap <- function(mapping = NULL, data = NULL, geom = "raster",
                             position = "identity", ...,
-                            fun = "count", fun.args = list(),
+                            fun = NULL, fun.args = list(),
                             na.rm = FALSE, show.legend = NA,
                             inherit.aes = TRUE) {
   ggplot2::layer(
@@ -38,7 +56,11 @@ stat_ip_heatmap <- function(mapping = NULL, data = NULL, geom = "raster",
 StatIpHeatmap <- ggplot2::ggproto("StatIpHeatmap", ggplot2::Stat,
   required_aes = c("x", "y"),
 
-  default_aes = ggplot2::aes(z = NULL, fill = ggplot2::after_stat(value)),
+  default_aes = ggplot2::aes(
+    z = NULL,
+    ip = NULL,
+    fill = ggplot2::after_stat(value)
+    ),
 
   extra_params = c(
     "na.rm",
@@ -56,18 +78,18 @@ StatIpHeatmap <- ggplot2::ggproto("StatIpHeatmap", ggplot2::Stat,
   },
 
   compute_group = function(data, scales, coord,
-                           fun = "count", fun.args = list(), ...) {
-    summarize_grid(data, coord = coord, fun = fun, fun.args = fun.args)
+                           fun = NULL, fun.args = list(), ...) {
+    compute_ip_heatmap(data, coord = coord, fun = fun, fun.args = fun.args)
   }
 )
 
-summarize_grid <- function(data, coord, fun, fun.args) {
+compute_ip_heatmap <- function(data, coord, fun, fun.args) {
   # support formula interface
   if (is_formula(fun)) {
     fun <- as_function(fun)
   }
 
-  summarize_count <- is_scalar_character(fun) && fun == "count"
+  summarize_count <- is.null(fun)
   if (!summarize_count && !("z" %in% colnames(data))) {
     abort("stat_ip_heatmap() requires `z` aesthetic when using non-default summary function")
   }
@@ -75,22 +97,21 @@ summarize_grid <- function(data, coord, fun, fun.args) {
   # summarize grid found in data
   index <- list(x = data$x, y = data$y)
   labels <- lapply(index, function(x) sort(unique(x)))
-  grps <- if (summarize_count) {
-    split(data$x, index)
-  } else {
-    split(data$z, index)
-  }
-  names(grps) <- NULL
-
   out <- expand.grid(labels, KEEP.OUT.ATTRS = FALSE)
-  out$count <- vapply(grps, length, integer(1))
-  bits_per_pixel <- max_prefix_length(coord$canvas_network) - coord$pixel_prefix
-  out$proportion <- out$count / (2^bits_per_pixel)
+
+  out$count <- summarize_grid(data$x, index, length)
   out$value <- if (summarize_count) {
     out$count
   } else {
     f <- function(x) do.call(fun, c(list(quote(x)), fun.args))
-    unlist(lapply(grps, f))
+    summarize_grid(data$z, index, f)
+  }
+  if ("ip" %in% colnames(data)) {
+    f <- function(x) length(unique(x))
+    out$address_count <- summarize_grid(data$ip, index, f)
+
+    bits_per_pixel <- max_prefix_length(coord$canvas_network) - coord$pixel_prefix
+    out$address_proportion <- out$address_count / (2^bits_per_pixel)
   }
 
   # fill remaining grid so raster works
@@ -100,4 +121,10 @@ summarize_grid <- function(data, coord, fun, fun.args) {
     fill_na$value <- 0
   }
   tidyr::complete(out, tidyr::expand(out, x = range, y = range), fill = fill_na)
+}
+
+summarize_grid <- function(x, index, fun) {
+  grps <- split(x, index)
+  names(grps) <- NULL
+  unlist(lapply(grps, fun))
 }
