c7ea9337a907629e056bfe267a096fe59eb24d58
David Hall <david.hall.physics@gmail.com>
2020-09-03 19:12:57 -0700

    Additional tests

--- tests/testthat/test-error.R
+++ tests/testthat/test-error.R
@@ -7,6 +7,8 @@ test_that("stop_bad_aes_type() generates useful error message", {
 test_that("stop_missing_aes() generates useful error message", {
   verify_output(test_path("test-stop-missing-aes.txt"), {
     stop_missing_aes("my_layer", "ip")
+    stop_missing_aes("my_layer", c("x", "y"))
+    stop_missing_aes("my_layer", c("x", "y", "ip"))
   })
 })
 
