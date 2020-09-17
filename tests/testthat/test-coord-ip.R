48f0d48481e6a09d3f3d8ca5551728000b1e63bd
David Hall <david.hall.physics@gmail.com>
2020-09-17 07:57:01 -0700

    Different change crept in

--- tests/testthat/test-coord-ip.R
+++ tests/testthat/test-coord-ip.R
@@ -3,8 +3,3 @@ test_that("ipaddress classes passed through ggplot unscaled", {
   expect_equal(scale_type(ip_network()), "identity")
   expect_equal(scale_type(ip_interface()), "identity")
 })
-
-test_that("coords classes passed through ggplot unscaled", {
-  expect_equal(scale_type(ip_address_coords()), "identity")
-  expect_equal(scale_type(ip_network_coords()), "identity")
-})
