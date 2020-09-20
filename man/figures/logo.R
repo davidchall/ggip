0cc59d09396655a09a2120953274f54d05d5346f
David Hall <david.hall.physics@gmail.com>
2020-09-20 08:42:47 -0700

    Update logo palette

--- man/figures/logo.R
+++ man/figures/logo.R
@@ -3,16 +3,15 @@ library(ggip)
 library(sysfonts)
 
 
-ggip_logo <- function(outfile,
-                      text_color = "#ffea00", line_color = "#7dc06f", bg_color = "#3f1f5f",
-                      text_font = "Ubuntu Mono") {
+ggip_logo <- function(outfile, text_color = "#f79d1e", text_font = "Ubuntu Mono",
+                      line_color = "#6ee2ff", bg_color = "#000000", border_color = "#f79d1e") {
 
   font_add_google(text_font)
 
   p <- ggplot() +
     geom_hilbert_outline(curve_order = 3, color = line_color) +
     coord_ip() +
-    theme(panel.background = element_rect(fill = bg_color))
+    theme_transparent()
 
   sticker(
     # subplot
@@ -20,7 +19,7 @@ ggip_logo <- function(outfile,
     # package name
     package = "ggip", p_family = text_font, p_color = text_color, p_size = 8.5, p_y = 1.18,
     # border
-    h_color = "#595959", white_around_sticker = TRUE,
+    h_color = border_color, h_fill = bg_color,
     # output file
     filename = outfile
   )
